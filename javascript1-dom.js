bf44318b59a8df920e0eeaceb3a537e40f11fb6c
newlec <newlec@namoolab.com>
2019-02-05 00:55:22 +0900

    Ex10-제목을 클릭하면 데이터 정렬하기

--- javascript1-dom.js
+++ javascript1-dom.js
@@ -1,3 +1,49 @@
+//Ex10-클릭한 컬럼을 기준으로 레코드 정렬하기 #1
+window.addEventListener("load", function(){
+
+    var notices = [
+        {"id":1, "title":"유투브에 끌려다니지 않으려고 했는데....ㅜㅜ..", "regDate":"2019-02-05", "writerId":"newlec", "hit":2},
+        {"id":2, "title":"자바스크립트란..", "regDate":"2019-02-02", "writerId":"newlec", "hit":0},
+        {"id":3, "title":"기본기가 튼튼해야....", "regDate":"2019-02-01", "writerId":"newlec", "hit":1},
+        {"id":4, "title":"근데 조회수가 ㅜㅜ..", "regDate":"2019-01-25", "writerId":"newlec", "hit":0}
+    ];
+
+    var section = document.querySelector("#section10");
+    
+    var noticeList =section.querySelector(".notice-list");
+    var titldTd = section.querySelector(".title");
+    var tbodyNode = noticeList.querySelector("tbody");
+
+    var bindData = function(){
+        var template = section.querySelector("template");
+
+        for(var i=0; i<notices.length; i++){
+            var cloneNode = document.importNode(template.content, true);
+            var tds = cloneNode.querySelectorAll("td");
+            tds[0].textContent = notices[i].id;            
+
+            var aNode = tds[1].children[0];
+            aNode.href=notices[i].id;
+            aNode.textContent = notices[i].title;
+
+            tds[2].textContent = notices[i].regDate;
+            tds[3].textContent = notices[i].writerId;
+            tds[4].textContent = notices[i].hit;
+
+            tbodyNode.appendChild(cloneNode);
+        }
+    };
+
+    bindData();
+
+    var titleSorted = false;
+
+    titldTd.onclick = function(){
+        // 이 부분에 새로 배운 내용을 작성해 보세요.
+       
+    };
+});
+
 //Ex9-다중 노드선택 방법과 일괄삭제, 노드의 자리바꾸기
 window.addEventListener("load", function(){
 
@@ -9,7 +55,7 @@ window.addEventListener("load", function(){
     var delButton = section.querySelector(".del-button");
     var swapButton = section.querySelector(".swap-button");
 
-    allCheckbox.onchange = function(){    
+    allCheckbox.onchange = function(){
         
     };
 
@@ -18,6 +64,7 @@ window.addEventListener("load", function(){
     };
 
     swapButton.onclick = function(){
+                
         
     };
 
