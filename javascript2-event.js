9de7930a6d2e42a954f9bb96a53f92ac2d2cde78
newlec <newlec@namoolab.com>
2019-02-11 12:46:58 +0900

    event target 속성 다루기

---
+++ javascript2-event.js
@@ -0,0 +1,21 @@
+//연습문제 1-선택된 레코드 삭제하기:event target
+window.addEventListener("load", function(){
+
+
+    
+}); 
+
+
+
+//Ex1-선택된 이미지 보여주기:event target
+window.addEventListener("load", function(){
+
+    var section = document.querySelector("#section1");
+    
+    var imgs = section.querySelectorAll(".img");
+    var currentImg = section.querySelector(".current-img");
+    
+    
+    
+
+}); 
