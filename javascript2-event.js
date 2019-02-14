9dcbb2e7166fb9c926b83346e8fc0373b61eb740
newlec <newlec@namoolab.com>
2019-02-15 01:43:17 +0900

    Ex3-이벤트 버블링 중지하기

--- javascript2-event.js
+++ javascript2-event.js
@@ -1,5 +1,21 @@
-//연습문제 2-이벤트 버블링을 이용해 사용자 이벤트 처리하기:event Bubbling
+// Ex3-이벤트 버블링 멈추기
+window.addEventListener("load", function(){
 
+    var section = document.querySelector("#section3");
+    
+    var imgList = section.querySelector(".img-list"); 
+    var addButton = section.querySelector(".add-button");
+    var currentImg = section.querySelector(".current-img");
+    
+    imgList.onclick = function(e){
+        
+    };
+
+    addButton.onclick = function(e){
+        
+    };
+
+}); 
 
 
 
@@ -8,7 +24,7 @@
 //Ex2-이벤트 버블링을 이용해 사용자 이벤트 처리하기:event Bubbling
 window.addEventListener("load", function(){
 
-    var section = document.querySelector("#section3");
+    var section = document.querySelector("#section2");
     var imgList = section.querySelector(".img-list"); 
     var currentImg = section.querySelector(".current-img");
     
